e28e8aa37638fbbc959bcc1c41ef3ff69a787442
Pavol Loffay <p.loffay@gmail.com>
2015-04-12 15:47:23 +0200

    wsdl contract

--- ass2-ws/src/main/java/dst/ass2/ws/session/interfaces/ILectureStatisticsBean.java
+++ ass2-ws/src/main/java/dst/ass2/ws/session/interfaces/ILectureStatisticsBean.java
@@ -15,21 +15,21 @@ import javax.jws.soap.SOAPBinding;
  * This is the interface of the statistics Web Service.
  */
 @WebService(name = Constants.NAME,
-		portName = Constants.PORT_NAME,
-		targetNamespace = Constants.NAMESPACE,
-		serviceName = Constants.SERVICE_NAME)
+    portName = Constants.PORT_NAME,
+    targetNamespace = Constants.NAMESPACE,
+    serviceName = Constants.SERVICE_NAME)
+@SOAPBinding
 public interface ILectureStatisticsBean {
 
-	/**
-	 * Get statistics for a given platform.
-	 * @param request      The request object with parameters
-	 * @param platformName The name of the platform
-	 * @return statistics for the platform with the specified name.
-	 */
-	@WebMethod(operationName = Constants.OP_GET_STATS)
-	IGetStatsResponse getStatisticsForPlatform(
-			IGetStatsRequest request,
-			@WebParam(name = "name", header = true, partName = "name") String platformName)
-			throws WebServiceException;
-
+    /**
+     * Get statistics for a given platform.
+     * @param request      The request object with parameters
+     * @param platformName The name of the platform
+     * @return statistics for the platform with the specified name.
+     */
+    @WebMethod(operationName = Constants.OP_GET_STATS)
+    IGetStatsResponse getStatisticsForPlatform(
+            IGetStatsRequest request,
+            @WebParam(name = "name", header = true, partName = "name") String platformName)
+            throws WebServiceException;
 }
